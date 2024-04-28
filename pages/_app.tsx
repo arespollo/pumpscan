e8d234e54983b5f3b1c9a3c216042156d613d91f
arespollo <arespollo@outlook.com>
2024-04-29 07:23:01 +0800

    update: fix build bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
-import theme from "./theme/themeConfig";
+import theme from "../theme/themeConfig";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
